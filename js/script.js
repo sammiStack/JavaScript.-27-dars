0dd8299cca68f87216a2239a33e36c812e6bbc7a
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-09-27 21:34:25 +0200

    Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* -=- Darsga Topshiriq
+
+1) list of news knopkani ochirip tashlang
+
+2) 'BBC' yozuvni orniga 'UzNews'ga almashtiring va rangini oziz istagan rangni tanglang
+
+3) glavniy fon dagi rasimni ozgartiring. Rasim joylashuvi 'img' papkani ichida
+
+4) bizlarda yangiliklardan tashkil topgan massiv bor. 
+Yangiliklarni massiv yordamida chiqaring. Html dan ochirip Faqat JS code yozip.
+
+5) yangiliklarni oldiga ularni raqamlarini qoyip chiqimg (1, 2, 3)
+
+6) read more knopkaga border-radius style qoshing
+
+*/
+
+"use strict";
+
+const news = [
+  "FOOTBALL",
+  "BASKETBALL",
+  "UFC",
+  "BOX",
+  "AMERICAN FOOTBAL IN EU...",
+];
